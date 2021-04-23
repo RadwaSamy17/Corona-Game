0b7e957002fa6cd2ee32e8e1c8d13e0fc11cfac3
KhalidGad <khaled.626528@gmail.com>
2021-04-24 01:16:34 +0200

    Add assets files

---
+++ Assets/Level_1,2/scripts/restartlevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartlevel : MonoBehaviour
+{
+    [SerializeField]
+     
+    // restart the level if any red blood cells collide with the player 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag=="Player")
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Level_3/Level3/Scripts/TryAgainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TryAgainMenu : MonoBehaviour
+{
+    // easy to Understand ^_^.
+    public GameManger gameManger;
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        // call the losing video.
+        gameManger.GameOver();
+    }
+
+}
