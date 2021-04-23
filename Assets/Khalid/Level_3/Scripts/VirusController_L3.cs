929a25ac920c72d1fb9584e631fef0edcb9051eb
KhalidGad <khaled.626528@gmail.com>
2021-04-24 00:09:12 +0200

    Adding assets files

---
+++ Assets/Khalid/Level_3/Scripts/VirusController_L3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusController_L3 : MonoBehaviour
+{
+    public float speed = 7f;
+    public int damage = 2;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // to Make Our Virus Move Forward when it Instantiated from ShootingPoint on InfectedCell.
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    // When Object Thouch the Virus or our Virus Touch an Object.
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if That Object isn't the InfectCell then destroy our Virus.
+
+        if(!(collision.collider.CompareTag("InfectedCell")))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Radwa/scripts/tryagain0.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class tryagain0 : MonoBehaviour
+{
+    public GameObject completeLevelUI;
+    public GameObject tryLevelUI;
+    public void level2loader()
+    {
+          
+        completeLevelUI.SetActive(false);
+    }
+    public void level2loader2()
+    {
+        SceneManager.LoadScene("level2");
+        tryLevelUI.SetActive(false);
+    }
+}
